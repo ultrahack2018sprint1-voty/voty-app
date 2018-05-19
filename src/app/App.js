fd3a015717debbe836b7fceaae92486d6852a9f8
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2018-05-19 18:16:23 +0300

    Reorganise the files structure

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Welcome from '../welcome/Welcome';
+import Settings from '../settings/Settings';
+import Question from '../question/Question';
 
 import styles from './App.css';
 
@@ -7,7 +12,16 @@ require('dotenv').config();
 class App extends Component {
   render() {
     return (
-      <div className={styles.logo} />
+      <div className={styles.wrapper}>
+        <div className={styles.logo} />
+        <BrowserRouter>
+          <div className={styles.contentWrapper}>
+            <Route path="/" component={Welcome} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/question" component={Question} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
 
 import App from './app/App';
-import Welcome from './welcome/Welcome';
-import Settings from './settings/Settings';
-import Question from './question/Question';
-
-import styles from './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div className={styles.wrapper}>
-      <Route path="/" component={App} />
-      <Route path="/welcome" component={Welcome} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/question" component={Question} />
-    </div>
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root')
 );
-
-registerServiceWorker();
